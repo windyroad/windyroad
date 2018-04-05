cfbe38a794595d93e190f3f99bf97b7acb787102
Tom Howard <tom@windyroad.com.au>
2018-04-05 18:24:53 +1000

    Tidying buttons

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'gatsby-link'
-
+import './index.css'
 
 export default (props) => (
     <a href="#contact" className="button special" style={props.style}>{props.children}</a>
