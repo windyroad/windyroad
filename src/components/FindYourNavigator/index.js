3ae9ce04d87a01200c1940e43367620b8091cc27
Tom Howard <tom@windyroad.com.au>
2018-04-10 18:35:15 +1000

    scroll to contact

--- src/components/FindYourNavigator/index.js
+++ src/components/FindYourNavigator/index.js
@@ -1,22 +1,33 @@
 import React from 'react'
-import Link from 'gatsby-link'
 import './index.css'
 import FontAwesome from 'react-fontawesome'
 import Button from '../Button'
+import {
+  Link,
+  DirectLink,
+  Element,
+  Events,
+  animateScroll as scroll,
+  scrollSpy,
+  scroller,
+} from 'react-scroll'
 
 export default props => (
-  <Button
-    style={{
-      fontWeight: '900',
-      verticalAlign: 'middle',
-    }}
+  <Link
+    className="special button"
+    to="contact"
+    spy={true}
+    smooth={true}
+    hashSpy={true}
+    duration={1000}
   >
-    Find your navigator{' '}
+    Find your navigator
     <FontAwesome
       name="random"
       style={{
         verticalAlign: 'middle',
+        paddingLeft: '0.5em',
       }}
     />
-  </Button>
+  </Link>
 )
