3e891ffe153a8b0b05b0f66cd706e10559ace9d4
Tom Howard <tom@windyroad.com.au>
2018-05-25 18:26:11 +1000

    fixing release process

--- scripts/prerelease.js
+++ scripts/prerelease.js
@@ -1,5 +1,5 @@
 const releases = require('./releases');
 
-releases.cretePreRelease('windyroad', 'windyroad', process.argv[2]);
+releases.cretePreRelease('windyroad', 'windyroad', process.argv[2], process.argv[3], process.argv[4]);
 
 
