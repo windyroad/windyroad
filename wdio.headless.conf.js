046522e3a328a9787cf1c036fb33c6820ceec6ef
Tom Howard <tom@windyroad.com.au>
2018-06-25 15:00:44 +1000

    WIP on headless testing on CircleCI

--- wdio.headless.conf.js
+++ wdio.headless.conf.js
@@ -2,17 +2,8 @@ const defaults = require('./wdio.conf.js').config
 
 // clone prod config and add new properties/overrides
 const localConfig = Object.assign(defaults, {
-  capabilities: [
-    {
-      browserName: 'chrome',
-      chromeOptions: {
-        args: ['--window-size=1280,800', '--disable-gpu', '--headless'],
-      },
-    },
-  ],
-  path: '/',
-  port: '9515',
-  services: ['chromedriver'],
+  capabilities: [{ browserName: 'phantomjs' }],
+  services: ['phantomjs'],
 })
 
 // delete any unwanted properties delete localConfig.user; delete
