dbf6d93871650a5cecb84d3afce9d16c6bb4e774
Tom Howard <tom@windyroad.com.au>
2018-06-26 08:40:54 +1000

    tidy

--- wdio.dev.safari.conf.js
+++ wdio.dev.safari.conf.js
@@ -1,19 +1,17 @@
-const defaults = require("./wdio.conf.js").config;
+const defaults = require('./wdio.conf.js').config
 
 // clone prod config and add new properties/overrides
-var localConfig = Object.assign(defaults, {
-    capabilities: [{
-         browserName: 'chrome'
-    }],
-    baseUrl: 'http://localhost:8000',
-    path: '/',
-    port: '9515',
-    services: ['chromedriver'],
-});
+const localConfig = Object.assign(defaults, {
+  capabilities: [
+    {
+      browserName: 'safari',
+    },
+  ],
+  baseUrl: 'http://localhost:8000',
+  services: ['selenium-standalone'],
+})
 
-// delete any unwanted properties
-// delete localConfig.user;
-// delete localConfig.key;
-// delete localConfig.sauceConnect;
+// delete any unwanted properties delete localConfig.user; delete
+// localConfig.key; delete localConfig.sauceConnect;
 
-exports.config = localConfig;
+exports.config = localConfig
