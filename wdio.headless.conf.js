6802c7fe040aa68b7c7a64c3bc8f150cf346f719
Tom Howard <tom@windyroad.com.au>
2018-06-26 13:05:24 +1000

    using chome headless for headless testing
    phantomjs doesn't run the componentDidMount script for some reason

--- wdio.headless.conf.js
+++ wdio.headless.conf.js
@@ -2,10 +2,20 @@ const defaults = require('./wdio.conf.js').config
 
 // clone prod config and add new properties/overrides
 const localConfig = Object.assign(defaults, {
-  capabilities: [{ browserName: 'phantomjs' }],
-  services: ['phantomjs'],
+  capabilities: [
+    {
+      browserName: 'chrome',
+      chromeOptions: {
+        args: ['--headless', '--disable-gpu', '--window-size=1280,800']
+      }
+    }
+  ],
+  path: '/',
+  port: '9515',
+  services: ['chromedriver']
 })
 
+localConfig.cucumberOpts.tags = []
 // delete any unwanted properties delete localConfig.user; delete
 // localConfig.key; delete localConfig.sauceConnect;
 
