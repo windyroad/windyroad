686a3acc524c168fa60ddf3d7bfd6277058ddc7a
Tom Howard <tom@windyroad.com.au>
2019-03-12 21:02:53 +1100

    fixed release script

--- scripts/releases.js
+++ scripts/releases.js
@@ -1,14 +1,25 @@
-const axios = require('axios').create({
-  baseURL: 'https://api.github.com',
-  headers: {
-    authorization: `token ${process.env.GITHUB_TOKEN}`,
-  },
-  maxBodyLength: 100 * 1024 * 1024,
+const got = require('got');
+const Keyv = require('keyv');
+const KeyvFile = require('keyv-file');
+
+const cacheFile = new Keyv({
+  store: new KeyvFile({
+    filename: `./target/keyv-release.json`, // the file path to store the data
+    expiredCheckDelay: 24 * 3600 * 1000, // ms, check and remove expired data in each ms
+    writeDelay: 0, // ms, batch write to disk in a specific duration, enhance write performance.
+  }),
 });
 
-var followRedirects = require('follow-redirects');
-
-followRedirects.maxBodyLength = 100 * 1024 * 1024; // 100 MB
+const gotOptions = {
+  baseUrl: 'https://api.github.com',
+  headers: {
+    authorization: `token ${process.env.GITHUB_TOKEN}`,
+    'User-Agent': 'tompahoward',
+  },
+  resolveWithFullResponse: true,
+  json: true,
+  cache: cacheFile,
+};
 
 const FormData = require('form-data');
 const fs = require('fs');
@@ -17,8 +28,8 @@ const path = require('path');
 
 async function getReleases(owner, repo) {
   try {
-    let response = await axios.get(`/repos/${owner}/${repo}/releases`);
-    return response.data;
+    let response = await got(`/repos/${owner}/${repo}/releases`, gotOptions);
+    return response.body;
   } catch (err) {
     throw new Error(err);
   }
@@ -45,39 +56,44 @@ async function cretePreRelease(
       }
       nextVersion = `${major}.${minor}.${point}`;
     }
+    console.log('next version', nextVersion);
     fs.writeFileSync(outputVersionFile, nextVersion);
-    let createResponse = await axios.post(`/repos/${owner}/${repo}/releases`, {
-      tag_name: nextVersion,
-      name: `v${nextVersion}`,
-      body: `v${nextVersion}`,
-      draft: process.env.CI == null,
-      prerelease: true,
-    });
-    console.log(createResponse.data);
+    let createResponse = await got.post(
+      `/repos/${owner}/${repo}/releases`,
+      Object.assign({}, gotOptions, {
+        body: {
+          tag_name: nextVersion,
+          name: `v${nextVersion}`,
+          body: `v${nextVersion}`,
+          draft: process.env.CI == null,
+          prerelease: true,
+        },
+      }),
+    );
+    console.log(createResponse.body);
     let uploadUrl = urlTemplate
-      .parse(createResponse.data.upload_url)
+      .parse(createResponse.body.upload_url)
       .expand({ name: path.basename(artifactFile), label: '' });
-    let formData = new FormData();
-    fs.readFile(artifactFile, async (err, data) => {
-      try {
-        if (err) throw err;
-        formData.append('file', data);
-        let uploadResponse = await axios.post(uploadUrl, data, {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
-        });
-        console.log(uploadResponse.data);
-        fs.writeFileSync(outputArtifactUrl, uploadResponse.data.url);
-        return uploadResponse.data ? uploadResponse.data.url : null;
-      } catch (err) {
-        console.error(err);
-        throw new Error(err);
-      }
-    });
+    let form = new FormData();
+
+    form.append('my_file', fs.createReadStream(artifactFile));
+
+    const uploadResponse = await got.post(
+      uploadUrl,
+      Object.assign({}, gotOptions, {
+        body: form,
+        json: false,
+      }),
+    );
+    const uploadResponseBody = uploadResponse.body
+      ? JSON.parse(uploadResponse.body)
+      : null;
+    console.log('upload response', uploadResponseBody);
+    fs.writeFileSync(outputArtifactUrl, uploadResponseBody.url);
+    return uploadResponse.body ? uploadResponseBody.url : null;
   } catch (err) {
     console.error(err);
-    throw new Error(err);
+    process.exit(1);
   }
 }
 
